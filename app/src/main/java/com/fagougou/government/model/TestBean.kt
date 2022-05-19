05ccef93c797efc49b87d9d9913576e1ea792e84
Chovy <601189170@qq.com>
2022-05-19 18:07:36 +0800

    5.19 崩溃修复

---
+++ app/src/main/java/com/fagougou/government/model/TestBean.kt
@@ -0,0 +1,11 @@
+package com.fagougou.xiaoben.utils
+
+data class TestBean (
+    var leftcontent:String="",
+    var rightcontent:String=""
+)
+
+data class ContentStlye (
+    var content:String="",
+    var type:String="",
+)
