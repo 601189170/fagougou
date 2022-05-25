6999b238a29e363d651a0d7aa5e3b4cb4ad65803
郭凡 <g39088902@gmail.com>
2022-05-25 16:16:18 +0800

    合并chovy修改

--- app/src/main/java/com/fagougou/government/chatPage/messageItem/MessageRect.kt
+++ app/src/main/java/com/fagougou/government/chatPage/messageItem/MessageRect.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -111,7 +112,14 @@ fun MessageRect(
                     }
                 }
                 ClickableText(
-                    annotatedString, style = MaterialTheme.typography.h5, onClick = { offset ->
+                    modifier = Modifier.padding(12.dp),
+                    text = annotatedString,
+                    style = TextStyle(
+                        color = textColor,
+                        fontSize = 24.sp,
+                        lineHeight=38.sp,
+                    ),
+                    onClick = { offset ->
                         annotatedString.getStringAnnotations("policy", offset, offset).firstOrNull()?.let {
                             ChatViewModel.getDefInfo(it.item);
                         }
